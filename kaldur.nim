1e3fcbb29c54c0b2b40eee4a26aea533322ac5c6
Alex Chistyakov <alexclear@gmail.com>
2016-11-06 05:13:26 +0300

    Start a thread

--- kaldur.nim
+++ kaldur.nim
@@ -1,7 +1,16 @@
-import jester, asyncdispatch, htmlgen
+import jester, asyncdispatch, htmlgen, os
+
+var
+  thr: Thread[void]
+
+proc collectOnCPUMetrics() {.thread.} =
+  while true:
+    write(stderr, "On-CPU...\n")
+    sleep(60000)
 
 routes:
   get "/":
     resp h1("Hello world")
 
+createThread(thr, collectOnCPUMetrics)
 runForever()
