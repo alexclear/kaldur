6a78e3952fca42280d87c95d9010a03a39791c1a
Alex Chistyakov <alexclear@gmail.com>
2016-11-08 03:00:08 +0300

    Fold the stacks

--- kaldur.nim
+++ kaldur.nim
@@ -3,14 +3,23 @@ import jester, asyncdispatch, htmlgen, os, osproc, times
 var
   thr: Thread[void]
 
+proc foldStacks(timestamp: int) {.thread.} =
+  write(stderr, "Folder... " & $timestamp & "\n")
+  let errCode = execCmd("perf script -i /var/lib/kaldur/perf" & $timestamp & ".data | /root/FlameGraph/stackcollapse-perf.pl > /var/lib/kaldur/out" & $timestamp & ".perf-folded")
+  write(stderr, "Folder finished, error code: " & $errCode & "\n")
+  if errCode != 0:
+    quit(QuitFailure)
+
 proc collectOnCPUMetrics() {.thread.} =
   while true:
+    var thr1: Thread[int]
     let currentTime = toInt(epochTime())
     write(stderr, "On-CPU... " & $currentTime & "\n")
     let errCode = execCmd("perf record -F 99 -o /var/lib/kaldur/perf" & $currentTime & ".data -a -g -- sleep 60")
     write(stderr, "Error code: " & $errCode & "\n")
     if errCode != 0:
       quit(QuitFailure)
+    createThread(thr1, foldStacks, currentTime)
 
 routes:
   get "/":
